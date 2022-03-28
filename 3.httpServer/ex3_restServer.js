a80a74cae93238b58b6367fb04a68bc87da13eea
seo077 <young102473@gmail.com>
2022-03-29 00:40:54 +0900

    3.httpServer

--- 3.httpServer/ex3_restServer.js
+++ 3.httpServer/ex3_restServer.js
@@ -16,6 +16,9 @@ http.createServer(async (req, res) => {
                 const data = await fs.readFile('./ex3_about.html');
                 res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
                 return res.end(data);
+            }else if(req.url === '/users'){
+                res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+                return res.end(JSON.stringify(users));
             }
             // 주소가 /도 /about도 아니면
             try {
@@ -23,6 +26,42 @@ http.createServer(async (req, res) => {
                 return res.end(data);
             } catch (error) {
                 // 주소에 해당하는 라우터를 못 찾음 404
+            }
+        }else if(req.method === 'POST'){
+            if(req.url === '/user'){
+                let body = '';
+                // 요청의 body를 stream 형식으로 받음.
+                req.on('data', (data) => {
+                    body += data;
+                });
+                // 요청의 body를 다 받은 후 실행됨.
+                return req.on('end', () =>{
+                    console.log('POST 본문(Body):', body);
+                    const {name} = JSON.parse(body);
+                    const id = Date.now();
+                    users[id] = name;
+                    res.writeHead(201);
+                    res.end('등록 성공');
+                });
+            }
+        }else if(req.method === 'PUT'){
+            if(req.url.startsWith('/user/')){
+                const key = req.url.split('/')[2];
+                let body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                return req.on('end', () => {
+                    console.log('PUT 본문(Body):', body);
+                    users[key] = JSON.parse(body).name;
+                    return res.end(JSON.stringify(users));
+                });
+            }
+        }else if(req.method === 'DELETE'){
+            if(req.url.startsWith('/user/')){
+                const key = req.url.split('/')[2];
+                delete users[key];
+                return res.end(JSON.stringify(users));
             }
         }
         res.writeHead(404);
